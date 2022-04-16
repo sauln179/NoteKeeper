48937fe820d4cae29d6979e98a9e23fca9a50d03
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-04-16 14:17:30 -0700

    Update saveData.js

--- db/saveData.js
+++ db/saveData.js
@@ -1,9 +1,11 @@
 // Dependecncies
 const util = require('util');
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid'); // Newest update
 
+//To create and keep track of ids of saved notes.
+const { v4: uuidv4 } = require('uuid');
 
+//Fs to read and write to file
 const readNote = util.promisify(fs.readFile);
 const writeNote = util.promisify(fs.writeFile);
 
@@ -11,11 +13,12 @@ class Save {
     write(note) {
         return writeNote('db/db.json', JSON.stringify(note));
     }
-
+    
+    //Reads what is in the database,
     read() {
         return readNote('db/db.json', 'utf8');
     }
-
+    //Retrieves based on what it ended up reading within db.json
     retrieveNotes() {
         return this.read().then(notes => {
             let parsedNotes;
@@ -33,7 +36,7 @@ class Save {
         if (!title || !text) {
             throw new Error('Both title and text can not be blank');
         }
-        // Use UUID package to add unique IDs
+        // Creates the id
         const newNote = { title, text, id: uuidv4() };
 
         // Retrieve Notes, add the new note, update notes
